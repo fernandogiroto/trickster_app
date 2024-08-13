9fdd1d576c9d8050f51adb7f3ecd64ccec24f546
fernandogiroto <giroto.luis@gmail.com>
2024-08-13 23:39:13 +0200

    HomeView Update

---
+++ src/components/AnimatedText.vue
@@ -0,0 +1,33 @@
+<template>
+	<span class="animated-text">{{ title }}</span>
+</template>
+  
+<script setup>
+
+	const props = defineProps({
+		title:String
+	})
+
+</script>
+  
+<style lang="scss" scoped>
+
+	@import '@/scss/animations';
+
+	.animated-text {
+		font: 50px "Oswald", sans-serif;
+		letter-spacing: 0;
+		padding: .25em 0 .325em;
+		display: block;
+		margin: 0 auto;
+		text-shadow: 0 0 80px rgba(255,255,255,.5);
+		background: url('../assets/img/bar.png') repeat-y;
+		-webkit-background-clip: text;
+		background-clip: text;
+		-webkit-text-fill-color: transparent;
+		-webkit-animation: aitf 80s linear infinite;
+		-webkit-transform: translate3d(0,0,0);
+		-webkit-backface-visibility: hidden;
+	}
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,28 +1,23 @@
 <template>
-  
   <div class="trickster-video">
     <video autoplay muted playsinline loop>
       <source src="@/assets/videos/trickster.mov" type="video/mp4">
       Seu navegador não suporta o elemento de vídeo.
     </video>
-    <Logo></Logo>
+    <AnimatedText title="O IMPOSTOR"/>
   </div>
   <div class="game-start">
     <input type="text"  v-model="username">
-  <button @click="addUser(username)">Adicionar Usuário</button>
-  <button @click="newGame()">Começar Jogo</button>
+    <button @click="addUser(username)">Adicionar Usuário</button>
+    <button @click="newGame()">Começar Jogo</button>
   </div>
-
-  
-
 </template>
-
 
 <script setup>
 
   import {ref} from 'vue';
   import { useGameStore } from '@/stores/game';
-  import Logo from '@/components/Logo.vue';
+  import AnimatedText from '@/components/AnimatedText.vue';
   import router from '@/router';
   const store = useGameStore();
 
@@ -41,21 +36,24 @@
  
 </script>
 
-<style>
-.trickster-video {
-  width: 100%; /* Ajusta a largura do contêiner pai */
-  height: 100%; /* Ajusta a altura do contêiner pai */
-  overflow: hidden; /* Garante que nada saia do contêiner */
-  position: relative; /* Necessário para posicionamento absoluto do vídeo */
-  display: flex;
-  flex-direction: column;
-}
+<style lang="scss">
 
-video {
-  top: 0; /* Alinha o vídeo ao topo do contêiner pai */
-  left: 0; /* Alinha o vídeo à esquerda do contêiner pai */
-  width: 100%; /* Faz com que o vídeo ocupe 100% da largura do contêiner pai */
-  height: 100%; /* Faz com que o vídeo ocupe 100% da altura do contêiner pai */
-  object-fit: cover; /* Faz com que o vídeo cubra o contêiner, mantendo a proporção */
-}
+  @import '@/scss/mixings';
+
+  .trickster-video {
+    @include flexbox(column,initial,initial);
+    width: 100%; 
+    height: 100%; 
+    overflow: hidden; 
+    position: relative; 
+  }
+
+  video {
+    top: 0; 
+    left: 0; 
+    width: 100%; 
+    height: 100%;
+    object-fit: cover; 
+  }
+
 </style>
