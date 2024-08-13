47dd1d724085cd7ead0da1a28e89a1aa59069b58
fernandogiroto <giroto.luis@gmail.com>
2024-08-13 23:14:43 +0200

    [HomeView]

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,20 @@
-
 <template>
-  <video autoplay muted playsinline>
+  
+  <div class="trickster-video">
+    <video autoplay muted playsinline loop>
       <source src="@/assets/videos/trickster.mov" type="video/mp4">
       Seu navegador não suporta o elemento de vídeo.
     </video>
-  Artista Impostor
-  <input type="text"  v-model="username">
+    <Logo></Logo>
+  </div>
+  <div class="game-start">
+    <input type="text"  v-model="username">
   <button @click="addUser(username)">Adicionar Usuário</button>
   <button @click="newGame()">Começar Jogo</button>
+  </div>
+
+  
+
 </template>
 
 
@@ -15,6 +22,7 @@
 
   import {ref} from 'vue';
   import { useGameStore } from '@/stores/game';
+  import Logo from '@/components/Logo.vue';
   import router from '@/router';
   const store = useGameStore();
 
@@ -33,3 +41,21 @@
  
 </script>
 
+<style>
+.trickster-video {
+  width: 100%; /* Ajusta a largura do contêiner pai */
+  height: 100%; /* Ajusta a altura do contêiner pai */
+  overflow: hidden; /* Garante que nada saia do contêiner */
+  position: relative; /* Necessário para posicionamento absoluto do vídeo */
+  display: flex;
+  flex-direction: column;
+}
+
+video {
+  top: 0; /* Alinha o vídeo ao topo do contêiner pai */
+  left: 0; /* Alinha o vídeo à esquerda do contêiner pai */
+  width: 100%; /* Faz com que o vídeo ocupe 100% da largura do contêiner pai */
+  height: 100%; /* Faz com que o vídeo ocupe 100% da altura do contêiner pai */
+  object-fit: cover; /* Faz com que o vídeo cubra o contêiner, mantendo a proporção */
+}
+</style>
